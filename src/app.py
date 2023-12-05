5dfb2e516b17c7e8973ef442504295cf45cb393e
Prakhya <saiprakhya.mylavaram@sjsu.edu>
2023-12-05 11:11:52 -0800

    pushed python backend

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from bson.errors import InvalidId
 import math
 from flask import Flask, request, jsonify, g
 from urllib.parse import quote_plus
@@ -6,6 +7,11 @@ from geojson import Point
 from flask_cors import CORS 
 from flask import Flask
 import requests
+from bson import ObjectId
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # Create a Flask application
 app = Flask(__name__)
@@ -28,12 +34,45 @@ def get_Lat_Long(location_query):
             return latitude, longitude
     return None, None
 
-#Google api for location?
+@app.route('/getStations', methods=['GET'])
+def fetch_stations():
+    stationID = 1583 
+
+    if stationID is None:
+        return jsonify({"error": "Station ID not provided"}), 400
+
+    db = client['ev_station_db']
+    collection = db['ev_stations']
+
+    query = {'ID': stationID}
+    station = collection.find_one(query)
+
+    if station:
+        # Process the station data
+        processed_station = {
+            "City": station.get("City"),
+            "State": station.get("State"),
+            "ZIP": station.get("ZIP"),
+            "Latitude": station.get("Latitude"),
+            "Longitude": station.get("Longitude"),
+            "Station_Name": station.get("Station Name"),
+            "Address": station.get("Street Address"),
+            "Num_Level_2": None if math.isnan(station.get("EV Level2 EVSE Num", math.nan)) else station["EV Level2 EVSE Num"],
+            "Num_level_1": None if math.isnan(station.get("EV Level1 EVSE Num", math.nan)) else station["EV Level1 EVSE Num"]
+        }
+
+        return jsonify(processed_station)
+    else:
+        return jsonify({"error": "Station not found"}), 404
+
+
+
 
 # Define the /search endpoint route
 @app.route('/search', methods=['GET'])
 def search_stations():
     #zipcode = request.args.get('zipcode')
+    station_name = request.args.get('stationName')
     location_query = request.args.get('location') 
     encoded_location = quote_plus(location_query)
     latitude, longitude = get_Lat_Long(location_query)
@@ -47,6 +86,31 @@ def search_stations():
     # Use the 'client' global variable to access the MongoDB connection
     db = client['ev_station_db']
     collection = db['ev_stations']
+
+    query = {}
+    if location_query:
+        encoded_location = quote_plus(location_query)
+        latitude, logitude = get_Lat_Long(location_query)
+        print('searching coordinates for ZIP', location_query)
+        print(latitude, longitude)
+
+        if latitude is None or longitude is None:
+            return jsonify({"error": "Unable to find location for the given zipcode"}), 400
+
+        location = Point((longitude, latitude))
+
+        collection.create_index([("location", "2dsphere")])
+
+        query["location"]={
+            "$nearSphere":{
+                "$geometry": location,
+                "$maxDistance": radius_meters
+            }
+        }
+    if station_name:
+        query["$or"]== [{"Station Name": {"$regex": station_name, "$options": "i"}}]
+
+    result= collection.find(query)
 
     # Create a GeoJSON Point with the coordinates
     location = Point((longitude, latitude))
@@ -78,10 +142,82 @@ def search_stations():
             "Station_Name": record["Station Name"],
             "Address": record["Street Address"],
             "Num_Level_2": None if math.isnan(record["EV Level2 EVSE Num"]) else record["EV Level2 EVSE Num"],
-            "Num_level_1": None if math.isnan(record["EV Level1 EVSE Num"]) else record["EV Level1 EVSE Num"]
+            "Num_level_1": None if math.isnan(record["EV Level1 EVSE Num"]) else record["EV Level1 EVSE Num"],
         }
         stations.append(station_info)
     return jsonify(stations)
+
+
+# @app.route('/stations/<station_id>/maintenance', methods=['PATCH'])
+# def update_maintenance(station_id):
+#     # Parse the JSON data from the request
+#     data = request.get_json()
+
+#     # Extract the maintenance status from the request data
+#     maintenance_status = data.get('maintenance', None)
+
+#     # Check if the maintenance status is provided
+#     if maintenance_status is None:
+#         return jsonify({"error": "Maintenance status not provided"}), 400
+
+#     # Use the 'client' global variable to access the MongoDB connection
+#     db = client['ev_station_db']
+#     collection = db['ev_stations']
+
+#     # Query using the 'ID' field
+    
+#     result = collection.update_one(
+#         {"ID": int(station_id)},  # Match the station by its ID
+#         {
+#             "$set": {
+#                 "maintenance": maintenance_status,
+#                 "availability": not maintenance_status
+#             }
+#         }
+#     )
+
+#     # Check if the update was successful
+#     if result.matched_count == 0:
+#         return jsonify({"error": "Station not found"}), 404
+#     elif result.modified_count == 0:
+#         return jsonify({"error": "Station status not modified"}), 409
+
+#     # Return success message
+#     return jsonify({"success": True, "message": "Station maintenance status updated"}), 200
+
+
+@app.route('/stations/<station_id>/maintenance', methods=['PATCH'])
+def update_maintenance(station_id):
+    data = request.get_json()
+    maintenance_status = data.get('maintenance', None)
+
+    if maintenance_status is None:
+        return jsonify({"error": "Maintenance status not provided"}), 400
+
+    try:
+        db = client['ev_station_db']
+        collection = db['ev_stations']
+
+        # Ensure station_id is an integer as it is in MongoDB
+        station_id_int = int(station_id)
+
+        # Update the maintenance status in MongoDB
+        result = collection.update_one(
+            {"ID": station_id_int},
+            {"$set": {"maintenance": maintenance_status}}
+        )
+
+        if result.matched_count == 0:
+            return jsonify({"error": "Station not found"}), 404
+        elif result.modified_count == 0:
+            return jsonify({"error": "No update made"}), 304  # Or another appropriate status code
+
+        return jsonify({"success": True, "message": "Station maintenance status updated"}), 200
+
+    except Exception as e:
+        print(f"Error updating maintenance status: {e}")
+        return jsonify({"error": str(e)}), 500
+
 
 
 # Run the Flask application
